dc27f68a92875418bd1dd9cdfe8d14bc5e8de39d
Nicolò <s222948@studenti.polito.it>
2018-05-02 10:49:31 +0200

    Interfaccia implementata

--- Lab07/src/it/polito/tdp/poweroutages/PowerOutagesController.java
+++ Lab07/src/it/polito/tdp/poweroutages/PowerOutagesController.java
@@ -1,6 +1,54 @@
 package it.polito.tdp.poweroutages;
 
+/**
+ * Sample Skeleton for 'PowerOutages.fxml' Controller Class
+ */
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class PowerOutagesController {
 
-	
+	@FXML // ResourceBundle that was given to the FXMLLoader
+	private ResourceBundle resources;
+
+	@FXML // URL location of the FXML file that was given to the FXMLLoader
+	private URL location;
+
+	@FXML // fx:id="choiceBox"
+	private ChoiceBox<?> choiceBox; // Value injected by FXMLLoader
+
+	@FXML // fx:id="yearArea"
+	private TextField yearArea; // Value injected by FXMLLoader
+
+	@FXML // fx:id="hoursArea"
+	private TextField hoursArea; // Value injected by FXMLLoader
+
+	@FXML // fx:id="buttonCalc"
+	private Button buttonCalc; // Value injected by FXMLLoader
+
+	@FXML // fx:id="resultArea"
+	private TextArea resultArea; // Value injected by FXMLLoader
+
+	@FXML
+	void handleCalc(ActionEvent event) {
+
+	}
+
+	@FXML // This method is called by the FXMLLoader when initialization is complete
+	void initialize() {
+		assert choiceBox != null : "fx:id=\"choiceBox\" was not injected: check your FXML file 'PowerOutages.fxml'.";
+		assert yearArea != null : "fx:id=\"yearArea\" was not injected: check your FXML file 'PowerOutages.fxml'.";
+		assert hoursArea != null : "fx:id=\"hoursArea\" was not injected: check your FXML file 'PowerOutages.fxml'.";
+		assert buttonCalc != null : "fx:id=\"buttonCalc\" was not injected: check your FXML file 'PowerOutages.fxml'.";
+		assert resultArea != null : "fx:id=\"resultArea\" was not injected: check your FXML file 'PowerOutages.fxml'.";
+
+	}
 }
